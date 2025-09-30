59d695e12c70af08233ee9ce64737e94d8b13f43
Morris <1216770@student.roc-nijmegen.nl>
2025-09-30 13:47:56 +0200

    test2

--- src/ovOOP/App.java
+++ src/ovOOP/App.java
@@ -1,6 +1,6 @@
 package ovOOP;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World 2!");
+        System.out.println("Hello, World 2! right?");
     }
 }
