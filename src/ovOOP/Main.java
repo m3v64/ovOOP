527a4850de851c987d2a13281b4149434c3b038b
M3v_ <max@m3v.dev>
2025-10-03 09:34:06 +0200

    added data file for json data

--- src/ovOOP/Main.java
+++ src/ovOOP/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Saldo.accountConf(args);
         Scanner scanner = new Scanner(System.in);
         Travel.startMenu(scanner);
     }
