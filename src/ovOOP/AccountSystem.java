b9cba8bfa0c7760d059cf109ee7f2edbe6d79e2c
M3v_ <max@m3v.dev>
2025-10-08 11:29:56 +0200

    updated colors and relocated outdated code

--- src/ovOOP/AccountSystem.java
+++ src/ovOOP/AccountSystem.java
@@ -11,19 +11,9 @@ import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 
 public class AccountSystem {
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
-
     static void displayAccounts(Scanner scanner) {
         System.out.println();
-        System.out.println(ANSI_CYAN + "Please choose from the following list");
+        System.out.println(Travel.ANSI_CYAN + "Please choose from the following list");
 
         int accounts = Option.showOption(scanner, "Login,Logout,Main Menu");
 
@@ -47,11 +37,11 @@ public class AccountSystem {
     }
     static void loginSystem(Scanner scanner) {
         System.out.println();
-        System.out.println(ANSI_CYAN + "Username:");
+        System.out.println(Travel.ANSI_CYAN + "Username:");
 
         String username = scanner.next();
 
-        System.out.println(ANSI_CYAN + "\nPassword:");
+        System.out.println(Travel.ANSI_CYAN + "\nPassword:");
         String password = scanner.next();
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
@@ -65,13 +55,13 @@ public class AccountSystem {
             for (Data d : dataList) {
                 if (d.getUsername().equalsIgnoreCase(username) && d.getPassword().equals(password)) {
                     Main.userID = d.getUserID();
-                    System.out.println(ANSI_GREEN + "you have logged in!");
+                    System.out.println(Travel.ANSI_GREEN + "you have logged in!");
                     break;
                 }
             }
 
             if (Main.userID == 0) {
-                System.out.println(ANSI_RED + "that account does not exist.");
+                System.out.println(Travel.ANSI_RED + "that account does not exist.");
             }
 
         } catch (Exception e) {
@@ -80,40 +70,4 @@ public class AccountSystem {
 
         Travel.startMenu(scanner);
     }
-
-    // static void signupSystem(Scanner scanner) {
-    //     System.out.println(ANSI_CYAN + "Please enter the name of the account you want to create:");
-    //     String username = scanner.next().toLowerCase();
-    //     System.out.println(ANSI_CYAN + "Please enter the password of the account you want to create");
-    //     String password = scanner.next();
-
-    //     File file = new File("data/Accounts.json");
-
-    //     Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-    //     List<Account> accounts = new ArrayList<>();
-
-    //     if (file.exists()) {
-    //         try (FileReader reader = new FileReader(file)) {
-    //             Type accountListType = new TypeToken<List<Account>>() {
-    //             }.getType();
-    //             accounts = gson.fromJson(reader, accountListType);
-    //             if (accounts == null) {
-    //                 accounts = new ArrayList<>();
-    //             }
-    //         } catch (Exception e) {
-    //             e.printStackTrace();
-    //         }
-    //     }
-
-    //     accounts.add(new Account(username, password, 0.00));
-    //     try (FileWriter writer = new FileWriter(file)) {
-    //         gson.toJson(accounts, writer);
-    //     } catch (Exception e) {
-    //         e.printStackTrace();
-    //     }
-    //     System.out.println(ANSI_GREEN + "Account created successfully!");
-    //     Travel.startMenu(scanner);
-    // }
-
 }
