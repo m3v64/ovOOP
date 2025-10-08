f94276731366e1e776ab958a68647d0517ace481
M3v_ <max@m3v.dev>
2025-10-08 13:23:21 +0200

    added loction object

--- src/ovOOP/Data.java
+++ src/ovOOP/Data.java
@@ -14,6 +14,7 @@ public class Data {
     private int userID;
     private String username;
     private String password;
+    private String location;
     private double balance;
 
     public Data(int userID) {
@@ -31,6 +32,7 @@ public class Data {
                 if (d.getUserID() == userID) {
                     this.username = d.username;
                     this.password = d.password;
+                    this.location = d.location;
                     this.balance = d.balance;
                     break;
                 }
@@ -43,6 +45,7 @@ public class Data {
         if (this.username == null) {
             this.username = "guest";
             this.password = "guest";
+            this.location = "Dryard";
             this.balance = 0.0;
         }
     }
@@ -50,18 +53,20 @@ public class Data {
     public int getUserID() {return userID;}
     public String getUsername() {return username;}
     public String getPassword() {return password;}
+    public String getLocation() {return location;}
     public double getBalance() {return balance;}
 
     public void setUserID(int userID) {this.userID = userID; updateJson();}
     public void setUsername(String username) {this.username = username; updateJson();}
     public void setPassword(String password) {this.password = password; updateJson();}
+    public void setLocation(String location) {this.location = location; updateJson();}
     public void setBalance(double balance) {this.balance = balance; updateJson();}
 
     private void updateJson() {
-        updateJson(this.userID, this.username, this.password, this.balance);
+        updateJson(this.userID, this.username, this.password, this.location, this.balance);
     }
 
-    public static void updateJson(int userID, String newUsername, String newPassword, double newBalance) {
+    public static void updateJson(int userID, String newUsername, String newPassword, String newLocation, double newBalance) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
         try (FileReader reader = new FileReader("data/Accounts.json")) {
@@ -74,6 +79,7 @@ public class Data {
                 if (d.getUserID() == userID) {
                     d.username = newUsername;
                     d.password = newPassword;
+                    d.location = newLocation;
                     d.balance = newBalance;
                     break;
                 }
