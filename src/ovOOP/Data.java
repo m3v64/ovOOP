1ab5a1562ac353f5985648e70beab86ff444a44c
M3v_ <max@m3v.dev>
2025-10-10 09:53:48 +0200

    started working on data.json for TrainLines get methods
    Co-authored-by: Morris433 <Morris433@users.noreply.github.com>

--- src/ovOOP/Data.java
+++ src/ovOOP/Data.java
@@ -8,6 +8,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.lang.reflect.Type;
 
 import java.util.Scanner;
@@ -19,16 +20,21 @@ public class Data {
     private String location;
     private double balance;
 
+    public Map<String, Map<String, Object>> connections;
+    public int line;
+
     public Data(int userID) {
         this.userID = userID;
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
         try (FileReader reader = new FileReader("data/AccountInfo.json")) {
-            Type dataListType = new TypeToken<List<Data>>(){}.getType();
+            Type dataListType = new TypeToken<List<Data>>() {
+            }.getType();
             List<Data> dataList = gson.fromJson(reader, dataListType);
 
-            if (dataList == null) dataList = new ArrayList<>();
+            if (dataList == null)
+                dataList = new ArrayList<>();
 
             for (Data d : dataList) {
                 if (d.getUserID() == userID) {
@@ -52,78 +58,111 @@ public class Data {
         }
     }
 
-    public int getUserID() {return userID;}
-    public String getUsername() {return username;}
-    public String getPassword() {return password;}
-    public String getLocation() {return location;}
-    public double getBalance() {return balance;}
-
-    public void setUserID(int userID) {this.userID = userID; updateJson();}
-    public void setUsername(String username) {this.username = username; updateJson();}
-    public void setPassword(String password) {this.password = password; updateJson();}
-    public void setLocation(String location) {this.location = location; updateJson();}
-    public void setBalance(double balance) {this.balance = balance; updateJson();}
-
-
-public static void addAccount(String username, String password, Scanner scanner) {
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    List<Data> dataList;
-
-    try (FileReader reader = new FileReader("data/AccountInfo.json")) {
-        Type dataListType = new TypeToken<List<Data>>() {}.getType();
-        dataList = gson.fromJson(reader, dataListType);
-        if (dataList == null) dataList = new ArrayList<>();
-    } catch (Exception e) {
-        // If file doesn't exist or can't be read, start fresh
-        dataList = new ArrayList<>();
+    public int getUserID() {
+        return userID;
     }
 
-    // get highest user id
-    int i = 0;
-    for (Data d : dataList) {
-        i ++;
+    public String getUsername() {
+        return username;
+    }
 
-        if (username.equalsIgnoreCase(d.username)) {
-            System.out.println("That username is already taken, Please choose another");
-            AccountSystem.displayAccounts(scanner);
-            return;
+    public String getPassword() {
+        return password;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+        updateJson();
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+        updateJson();
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+        updateJson();
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+        updateJson();
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+        updateJson();
+    }
+
+    public static void addAccount(String username, String password, Scanner scanner) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        List<Data> dataList;
+
+        try (FileReader reader = new FileReader("data/AccountInfo.json")) {
+            Type dataListType = new TypeToken<List<Data>>() {
+            }.getType();
+            dataList = gson.fromJson(reader, dataListType);
+            if (dataList == null)
+                dataList = new ArrayList<>();
+        } catch (Exception e) {
+            dataList = new ArrayList<>();
         }
+
+        int i = 0;
+        for (Data d : dataList) {
+            i++;
+
+            if (username.equalsIgnoreCase(d.username)) {
+                System.out.println("That username is already taken, Please choose another");
+                AccountSystem.displayAccounts(scanner);
+                return;
+            }
+        }
+
+        Data newUser = new Data(i);
+        newUser.username = username;
+        newUser.password = password;
+        newUser.location = "Giad";
+        newUser.balance = 0.0;
+
+        dataList.add(newUser);
+
+        try (FileWriter writer = new FileWriter("data/AccountInfo.json")) {
+            gson.toJson(dataList, writer);
+            System.out.println("New user added successfully!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Main.userID = i;
+
+        Menu.startMenu(scanner);
     }
-
-    // Create and add new user
-    Data newUser = new Data(i);
-    newUser.username = username;
-    newUser.password = password;
-    newUser.location = "Giad";
-    newUser.balance = 0.0;
-
-    dataList.add(newUser);
-
-    // Write updated list to file
-    try (FileWriter writer = new FileWriter("data/AccountInfo.json")) {
-        gson.toJson(dataList, writer);
-        System.out.println("New user added successfully!");
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
-
-    Main.userID = i;
-
-    Menu.startMenu(scanner);
-}
 
     private void updateJson() {
         updateJson(this.userID, this.username, this.password, this.location, this.balance);
     }
 
-    public static void updateJson(int userID, String newUsername, String newPassword, String newLocation, double newBalance) {
+    public static void updateJson(int userID, String newUsername, String newPassword, String newLocation,
+            double newBalance) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
         try (FileReader reader = new FileReader("data/AccountInfo.json")) {
-            Type dataListType = new TypeToken<List<Data>>(){}.getType();
+            Type dataListType = new TypeToken<List<Data>>() {
+            }.getType();
             List<Data> dataList = gson.fromJson(reader, dataListType);
 
-            if (dataList == null) dataList = new ArrayList<>();
+            if (dataList == null)
+                dataList = new ArrayList<>();
 
             for (Data d : dataList) {
                 if (d.getUserID() == userID) {
@@ -143,4 +182,40 @@ public static void addAccount(String username, String password, Scanner scanner)
             e.printStackTrace();
         }
     }
+
+    public static int[] listPossibleLines(String currentLocation) {
+        List<Integer> foundLines = new ArrayList<>();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        try (FileReader reader = new FileReader("data/TrainLines.json")) {
+            Type dataListType = new TypeToken<List<Data>>() {
+            }.getType();
+            List<Data> dataList = gson.fromJson(reader, dataListType);
+
+            if (dataList == null)
+                dataList = new ArrayList<>();
+
+            for (Data lineData : dataList) {
+                for (Map.Entry<String, Map<String, Object>> entry : lineData.connections.entrySet()) {
+                    String placeName = entry.getKey();
+                    if (placeName.equalsIgnoreCase(currentLocation)) {
+                        foundLines.add(lineData.line);
+                        break;
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Convert List<Integer> → int[]
+        int[] lines = new int[foundLines.size()];
+        for (int i = 0; i < foundLines.size(); i++) {
+            lines[i] = foundLines.get(i);
+        }
+
+        return lines;
+    }
+
 }
